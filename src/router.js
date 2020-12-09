10b9787cdb4046d6794f4537cdb551fa2b8def9e
Choirul R <choirul.rahmaditya@mail.ugm.ac.id>
2020-12-09 22:56:53 +0800

    [Choi] User Register and Get Profile

--- src/router.js
+++ src/router.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 const userController = require('./controllers/userController')
 const { authenticate } = require('./middlewares/auth')
 
+router.post('/user', userController.register)
 router.put('/user', userController.login)
+router.get('/user', authenticate, userController.profile)
 router.put('/topup', authenticate, userController.topUp)
 router.put('/transfer/:id', authenticate, userController.transfer)
 
